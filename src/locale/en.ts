807d67378d903609ab40aa742793e29290f8af6d
qww <497363953@qq.com>
2024-05-21 20:22:27 +0800

    feat: i18n

--- src/locale/en.ts
+++ src/locale/en.ts
@@ -1,5 +1,31 @@
 export const en = {
-	form: {
-		dataSource: "Data Source",
-	}
+    "form": {
+        "dataSource": "Data Source",
+        "dataRange": "Data Range",
+        "dataRangeExtra": "Dashboard filter has been applied",
+        "ratingFieldRequired": "A rating field must be selected",
+        "ratingField": "Rating",
+        "allData": "All data",
+        "needsAtLeastThreeRatings": "Rating field needs at least 3 scores",
+        "ratingFieldNeedToBeSelected": "A rating field must be selected",
+        "pleaseSelectRatingField": "Please select a rating field",
+        "showTable": "Show table"
+    },
+    "dataRange": {
+        "all": "All data"
+    },
+    "npsRole": {
+        "promoter": "Promoter",
+        "passive": "Passive",
+        "detractor": "Detractor"
+    },
+    "button": {
+        "confirm": "Confirm"
+    },
+    "table": {
+        "options": "Options",
+        "number": "Number selected",
+        "percent": "Percentage",
+        "score": "Score"
+    }
 }
--- src/locale/ja.ts
+++ src/locale/ja.ts
@@ -1,5 +1,31 @@
 export const ja = {
-	form: {
-		dataSource: "データソース",
+	"form": {
+		"dataSource": "データソース",
+		"dataRange": "データ範囲",
+		"dataRangeExtra": "ダッシュボードフィルタが適用されました",
+		"ratingFieldRequired": "評価フィールドを選択する必要があります",
+		"ratingField": "評価",
+		"allData": "すべてのデータ",
+		"needsAtLeastThreeRatings": "評価フィールドは少なくとも3つのスコアが必要です",
+		"ratingFieldNeedToBeSelected": "評価フィールドを選択する必要があります",
+		"pleaseSelectRatingField": "評価フィールドを選択してください",
+		"showTable": "表を表示"
+	},
+	"dataRange": {
+		"all": "すべてのデータ"
+	},
+	"npsRole": {
+		"promoter": "推進者",
+		"passive": "中立的",
+		"detractor": "批判者"
+	},
+	"button": {
+		"confirm": "確認"
+	},
+	"table": {
+		"options": "オプション",
+		"number": "選択数",
+		"percent": "割合",
+		"score": "スコア"
 	}
 }
